ff7018f3388f0209a59bb3246e0613283cfd516c
Kawai Leung (George) <georgeleung30@gmail.com>
2013-06-06 15:07:47 -0400

    THvector direction updated

--- Worm_pain_model/Preprocessing/nonlinear_mapping/nonlinear.m
+++ Worm_pain_model/Preprocessing/nonlinear_mapping/nonlinear.m
@@ -26,18 +26,27 @@ load(maxspeedfilename)
 
 %rescaling of the maximum speed such that the range of laser power are the
 %same
-tavgmaxspeed = (I_avgmaxspeed(end))/(avgmaxspeed(end))*avgmaxspeed;
+
+tavgmaxspeed = (I_avgmaxspeed(end))/(avgmaxspeed(end))*avgmaxspeed*0.7;
 
 %fitting a double exponential curve 
 curve = fit( tavgmaxspeed', I_avgmaxspeed' , 'exp2');
+
+%plot the curve
+figure(1)
 plot(curve)
+hold 
+plot(tavgmaxspeed', I_avgmaxspeed','.')
+ylabel('Original Laser power')
+xlabel('Transformed Laser power')
+hold
 
 %nI_ctrl = normalized laser power
 nI_ctrl = curve(I_ctrl)';
 
 
 
-%%%saving data
+%%%saving data to the same folder 
 cd(orgpath)
 save(datafilename, ['I' datafilename(6:10)],['nI' datafilename(6:10)],['nfspeed' datafilename(6:10)], ['speed' datafilename(6:10)])
 
